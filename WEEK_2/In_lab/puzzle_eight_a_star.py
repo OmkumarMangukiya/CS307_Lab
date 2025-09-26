a4469b41409805412ef7c9c92093ed5232c9486c
jaybhalodiya10 <jaybhalodiya109@gmail.com>
2025-09-26 14:01:14 +0530

    changes

--- WEEK_2/In_lab/puzzle_eight_a_star.py
+++ WEEK_2/In_lab/puzzle_eight_a_star.py
@@ -1,82 +1,302 @@
-import heapq
-import random
+import numpy as np
+import sys
+from time import time
+
 
 class Node:
-    def __init__(self, state, parent=None, g=0, h=0):
-        self.state = state
+    def __init__(self, parent, state, pcost, hcost):
         self.parent = parent
-        self.g = g
-        self.h = h
-        self.f = g + h
-    def __lt__(self, other):    
-        return self.f < other.f
+        self.state = state
+        self.pcost = pcost
+        self.hcost = hcost
+        self.cost = pcost + hcost
 
-def heuristic(node, goal_state):
-    h = sum(1 for i, val in enumerate(node.state) if val != 0 and val != goal_state[i])
-    return h
-def get_successors(node):
-    successors = []
-    index = node.state.index(0)
-    quotient = index//3
-    remainder = index%3
-    if quotient == 0:
-        moves = [3]
-    if quotient == 1:
-        moves = [-3, 3]
-    if quotient == 2:
-        moves = [-3]
-    if remainder == 0:
-        moves += [1]
-    if remainder == 1:
-        moves += [-1, 1]
-    if remainder == 2:
-        moves += [-1]
-    for move in moves:
-        im = index+move
-        if im >= 0 and im < 9:
-            new_state = list(node.state)
-            temp = new_state[im]
-            new_state[im] = new_state[index]
-            new_state[index] = temp
-            successor = Node(new_state, node, node.g+1)
-            successors.append(successor)            
-    return successors
+    def __hash__(self):
+        return hash("".join(self.state.flatten().astype(str)))
 
-def search_agent(start_state, goal_state):
-    start_node = Node(start_state)
-    goal_node = Node(goal_state)
-    frontier = []
-    heapq.heappush(frontier, (start_node.g, start_node))
-    visited = set()
-    nodes_explored = 0
-    while frontier:
-        _, node = heapq.heappop(frontier)
-        if tuple(node.state) in visited:
+    def __str__(self):
+        return str(self.state)
+
+    def __eq__(self, other):
+        return hash("".join(self.state.flatten().astype(str))) == hash(
+            "".join(other.state.flatten().astype(str))
+        )
+
+    def __ne__(self, other):
+        return hash("".join(self.state.flatten().astype(str))) != hash(
+            "".join(other.state.flatten().astype(str))
+        )
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.queue = []
+
+    def push(self, node):
+        self.queue.append(node)
+
+    def pop(self):
+        next_state = None
+        state_cost = 10**18
+        index = -1
+
+        for i in range(len(self.queue)):
+            if self.queue[i].cost < state_cost:
+                state_cost = self.queue[i].cost
+                index = i
+
+        return self.queue.pop(index)
+
+    def is_empty(self):
+        return len(self.queue) == 0
+
+    def __str__(self):
+        l = []
+        for i in self.queue:
+            l.append(i.state)
+
+        return str(l)
+
+    def __len__(self):
+        return len(self.queue)
+
+
+class Environment:
+    def __init__(self, depth=None, goal_state=None):
+        self.actions = [1, 2, 3, 4]  # 1 - Up, 2 - Down, 3 - Right, 4 - Left
+        self.goal_state = goal_state
+        self.depth = depth
+        self.start_state = self.generate_start_state()
+
+    def generate_start_state(self):
+        past_state = self.goal_state
+        i = 0
+        while i != self.depth:
+            new_states = self.get_next_states(past_state)
+            choice = np.random.randint(low=0, high=len(new_states))
+
+            if np.array_equal(new_states[choice], past_state):
+                continue
+
+            past_state = new_states[choice]
+            i += 1
+
+        return past_state
+
+    def get_start_state(self):
+        return self.start_state
+
+    def get_goal_state(self):
+        return self.goal_state
+
+    def get_next_states(self, state):
+        space = (0, 0)
+        for i in range(3):
+            for j in range(3):
+                if state[i, j] == 0:
+                    space = (i, j)
+                    break
+
+        new_states = []
+
+        if space[0] > 0:  # Move Up
+            new_state = np.copy(state)
+
+            val = new_state[space[0], space[1]]
+            new_state[space[0], space[1]] = new_state[space[0] - 1, space[1]]
+            new_state[space[0] - 1, space[1]] = val
+
+            new_states.append(new_state)
+
+        if space[0] < 2:  # Move down
+            new_state = np.copy(state)
+
+            val = new_state[space[0], space[1]]
+            new_state[space[0], space[1]] = new_state[space[0] + 1, space[1]]
+            new_state[space[0] + 1, space[1]] = val
+
+            new_states.append(new_state)
+
+        if space[1] < 2:  # Move right
+            new_state = np.copy(state)
+
+            val = new_state[space[0], space[1]]
+            new_state[space[0], space[1]] = new_state[space[0], space[1] + 1]
+            new_state[space[0], space[1] + 1] = val
+
+            new_states.append(new_state)
+
+        if space[1] > 0:  # Move Left
+            new_state = np.copy(state)
+
+            val = new_state[space[0], space[1]]
+            new_state[space[0], space[1]] = new_state[space[0], space[1] - 1]
+            new_state[space[0], space[1] - 1] = val
+
+            new_states.append(new_state)
+
+        return new_states
+
+    def reached_goal(self, state):
+        for i in range(3):
+            for j in range(3):
+                if state[i, j] != self.goal_state[i, j]:
+                    return False
+
+        return True
+
+
+class Agent:
+    def __init__(self, env, heuristic):
+        self.frontier = PriorityQueue()
+        self.explored = dict()
+        self.start_state = env.get_start_state()
+        self.goal_state = env.get_goal_state()
+        self.env = env
+        self.goal_node = None
+        self.heuristic = heuristic
+
+    def run(self):
+        init_node = Node(parent=None, state=self.start_state, pcost=0, hcost=0)
+        self.frontier.push(init_node)
+        steps = 0
+        while not self.frontier.is_empty():
+            curr_node = self.frontier.pop()
+            next_states = self.env.get_next_states(curr_node.state)
+
+            if hash(curr_node) in self.explored:
+                continue
+
+            self.explored[hash(curr_node)] = curr_node
+
+            if self.env.reached_goal(curr_node.state):
+                self.goal_node = curr_node
+                break
+            goal_state = self.env.get_goal_state()
+
+            l = []
+            for state in next_states:
+                hcost = self.heuristic(state, goal_state)
+                node = Node(
+                    parent=curr_node,
+                    state=state,
+                    pcost=curr_node.pcost + 1,
+                    hcost=hcost,
+                )
+                self.frontier.push(node)
+            steps += 1
+
+        return steps, self.soln_depth()
+
+    def soln_depth(self):
+        node = self.goal_node
+        count = 0
+        while node is not None:
+            node = node.parent
+            count += 1
+
+        return count
+
+    def print_nodes(self):
+        node = self.goal_node
+        l = []
+        while node is not None:
+            l.append(node)
+            node = node.parent
+
+        step = 1
+        for node in l[::-1]:
+            print("Step: ", step)
+            print(node)
+            step += 1
+
+    def get_memory(self):
+        mem = len(self.frontier) * 56 + len(self.explored) * 56
+        return mem
+
+
+def heuristic0(curr_state, goal_state):
+    return 0
+
+
+def heuristic1(curr_state, goal_state):
+    count = 0
+    for i in range(3):
+        for j in range(3):
+            if curr_state[i, j] != goal_state[i, j]:
+                count += 1
+
+    return count
+
+
+def heuristic2(curr_state, goal_state):
+    dist = 0
+
+    for i in range(3):
+        for j in range(3):
+            ele = curr_state[i, j]
+            if ele != 0:
+                goal_i, goal_j = np.where(goal_state == ele)
+                d = abs(goal_i[0] - i) + abs(goal_j[0] - j)
+                dist += d
+
+    return dist
+
+
+# Single instance test
+depth = 20
+goal_state = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]])
+env = Environment(depth, goal_state)
+print("Start State: ")
+print(env.get_start_state())
+print("Goal State: ")
+print(goal_state)
+
+agent = Agent(env=env, heuristic=heuristic2)
+steps, solution_depth = agent.run()
+print(f"Solution found in {solution_depth} steps!")
+print("\nSolution path:")
+agent.print_nodes()
+
+# Performance analysis
+print("\n" + "="*60)
+print("MEMORY AND TIME REQUIREMENTS FOR SOLVING PUZZLE-8")
+print("="*60)
+
+depths = [5, 10, 20, 50, 100, 150, 200, 250, 300]
+goal_state = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]])
+times_taken = {}
+mems = {}
+
+print("Depth (d)\tTime (seconds)\tMemory (bytes)")
+print("-"*50)
+
+for depth in depths:
+    time_taken = 0
+    mem = 0
+    iterations = 5  # Reduced iterations for higher depths
+    successful_runs = 0
+    
+    for i in range(iterations):
+        try:
+            env = Environment(depth=depth, goal_state=goal_state)
+            agent = Agent(env=env, heuristic=heuristic2)
+            start_time = time()
+            steps, solution_depth = agent.run()
+            end_time = time()
+            
+            if agent.goal_node is not None:  # Solution found
+                time_taken += end_time - start_time
+                mem += agent.get_memory()
+                successful_runs += 1
+        except:
             continue
-        visited.add(tuple(node.state))
-        if node.state == list(goal_node.state):
-            path = []
-            while node:
-                path.append(node.state)
-                node = node.parent
-            return path[::-1]
-        for successor in get_successors(node):
-            heapq.heappush(frontier, (successor.g, successor))
-    return None
-
-start_state = [1, 2, 3, 4, 5, 6, 7, 8, 0]
-s_node = Node(start_state)
-D = 20
-d = 0
-while d <= D:
-    goal_state = random.choice(list(get_successors(s_node))).state
-    s_node = Node(goal_state)
-    d = d+1
-
-solution = search_agent(start_state, goal_state)
-if solution:
-    print("Solution found:")
-    for step in solution:
-        print(step)
-else:
-    print("No solution found.")
+    
+    if successful_runs > 0:
+        time_taken /= successful_runs
+        mem = mem / successful_runs
+        times_taken[depth] = time_taken
+        mems[depth] = mem
+        print(f"{depth}\t\t{time_taken:.3f}\t\t{int(mem)}")
+    else:
+        print(f"{depth}\t\tTimeout\t\tN/A")
